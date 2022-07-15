9b1353541921d034f75fa5bc2903ff55f3c0bc91
unknown <dasilvadeoliveirafabio@gmail.com>
2022-07-14 21:02:42 -0300

    autenticação atualisada

---
+++ src/MaterialUI/UploadButtonsCadastro.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import Stack from '@mui/material/Stack';
+
+const Input = styled('input')({
+  display: 'none',
+});
+
+export default function UploadButtonsCadastro({setAvatar}) {
+  return (
+    <Stack direction="row" alignItems="center" spacing={2}>
+     
+      <label htmlFor="icon-button-file">
+        <Input accept="image/*" id="icon-button-file" type="file" onChange={e=>setAvatar(e.target.files[0])}/>
+        <IconButton color="primary" aria-label="upload picture" component="span">
+          <PhotoCamera />
+        </IconButton>
+      </label>
+    </Stack>
+  );
+}
